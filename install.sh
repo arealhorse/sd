5c12a603c3779be0d6b8116ecaf2adc12931b660
arealhorse <103963416+arealhorse@users.noreply.github.com>
2025-01-11 11:18:41 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 # One-liner to install this: curl -sSL https://raw.githubusercontent.com/arealhorse/sd/refs/heads/main/install.sh | bash
 
-# Disable the read-only filesystem
+echo "________STEP 1: Disable the read-only filesystem"
 sudo steamos-readonly disable
 
+echo "________STEP 2: Packman Key"
 sudo pacman-key --init
 sudo pacman-key --populate archlinux
 sudo pacman-key --populate holo
 
-# Download and verify the BlackArch installer script
+echo "________STEP 3: Download and verify the BlackArch installer script"
 curl -O https://blackarch.org/strap.sh
 echo "bbf0a0b838aed0ec05fff2d375dd17591cbdf8aa  strap.sh" | sha1sum -c
 
-# Make the installer script executable
+echo "________STEP 4: Make the installer script executable"
 chmod +x strap.sh
 
-# Run the installer script
+echo "________STEP 5: Run the installer script"
 sudo ./strap.sh
 
-# Enable multilib repository
+echo "________STEP 6: Enable multilib repository"
 sudo sed -i '/\[multilib\]/,/Include/s/^#//' /etc/pacman.conf
 
-# Synchronize and update package databases
+echo "________STEP 7: Sync"
 sudo pacman -Syu
